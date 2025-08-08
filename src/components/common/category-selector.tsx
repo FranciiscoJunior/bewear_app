c2914becfe009d7c1721d6b32d8d1e613f062d82
Francisco Júnior <franciscoamj1001@gmail.com>
2025-08-08 09:23:57 -0300

    faet: add category page

--- src/components/common/category-selector.tsx
+++ src/components/common/category-selector.tsx
@@ -1,25 +1,29 @@
-    import { categoryTable } from "@/db/schema";
+import { Link } from "lucide-react";
 
-    import { Button } from "../ui/button";
+import { categoryTable } from "@/db/schema";
 
-    interface CategorySelectorProps {
+import { Button } from "../ui/button";
+
+interface CategorySelectorProps {
     categories: (typeof categoryTable.$inferSelect)[];
-    }
+}
 
-    const CategorySelector = ({ categories }: CategorySelectorProps) => {
+const CategorySelector = ({ categories }: CategorySelectorProps) => {
     return (
         <div className="rounded-3xl bg-[#F4EFFF] p-6">
-        <div className="grid grid-cols-2 gap-3">
-            {categories.map((category) => (
-            <Button
-                key={category.id}
-                variant="ghost"
-                className="rounded-full bg-white text-xs font-semibold"
-            >
-                {category.name}
-            </Button>
-            ))}
-        </div>
+            <div className="grid grid-cols-2 gap-3">
+                {categories.map((category) => (
+                <Button
+                    key={category.id}
+                    variant="ghost"
+                    className="rounded-full bg-white text-xs font-semibold"
+                >
+                    {category.name}
+
+                    <Link href={`/category/${category.slug}`}>{category.name}</Link>
+                </Button>
+                ))}
+            </div>
         </div>
     );
 };
