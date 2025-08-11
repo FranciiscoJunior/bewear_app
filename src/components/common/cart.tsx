1c3190f2c09aef7c832bf27898f3e8445cc6fa99
Francisco Júnior <franciscoamj1001@gmail.com>
2025-08-11 09:43:02 -0300

    faet: ensure shipping address can created

--- src/components/common/cart.tsx
+++ src/components/common/cart.tsx
@@ -1,6 +1,6 @@
     "use client";
 
-    import { ShoppingBasketIcon } from "lucide-react";
+    import { Link, ShoppingBasketIcon } from "lucide-react";
 
     import { Button } from "@/components/ui/button";
     import { formatCentsToBRL } from "@/helpers/money";
@@ -77,7 +77,11 @@
                     <p>{formatCentsToBRL(cart?.totalPriceInCents ?? 0)}</p>
                 </div>
 
-                <Button className="mt-5 rounded-full">Finalizar compra</Button>
+                <Button className="mt-5 rounded-full" asChild>
+                    <Link href="/cart/identification">
+                        Finalizar compra
+                    </Link>
+                </Button>
                 </div>
             )}
             </div>
