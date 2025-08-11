9f3dc487e9fcf6d767a8df210965bf2978cec389
Francisco Júnior <franciscoamj1001@gmail.com>
2025-08-11 15:30:45 -0300

    faet: ensure cart shipping can be selected

--- src/app/cart/indentification/components/addresses.tsx
+++ src/app/cart/indentification/components/addresses.tsx
@@ -2,13 +2,12 @@
 
     import { zodResolver } from "@hookform/resolvers/zod";
     import { useRouter } from "next/navigation";
-    import { useEffect, useState } from "react";
+    import { useState } from "react";
     import { useForm } from "react-hook-form";
     import { PatternFormat } from "react-number-format";
     import { toast } from "sonner";
     import z from "zod";
 
-    import { getCart } from "@/actions/get-cart";
     import { Button } from "@/components/ui/button";
     import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
     import {
@@ -23,7 +22,7 @@
     import { Label } from "@/components/ui/label";
     import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
     import { shippingAddressTable } from "@/db/schema";
-    import { useCreateShippingAddress } from "@/hooks/mutations/use-create-shipping-address";
+    import { useCreateShippingAddress } from "@/hooks/mutations/use-create-shipping-adresses";
     import { useUpdateCartShippingAddress } from "@/hooks/mutations/use-update-cart-shipping-address";
     import { useCart } from "@/hooks/queries/use-cart";
     import { useUserAddresses } from "@/hooks/queries/use-user-addresses";
@@ -382,6 +381,6 @@
         </CardContent>
         </Card>
     );
-    };
+};
 
-    export default Addresses;
+export default Addresses;
